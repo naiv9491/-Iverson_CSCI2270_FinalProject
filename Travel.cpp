93e23684c984d5319969163067ae395df35369a1
naiv9491 <naomi.iverson@colorado.edu>
2015-04-27 23:49:01 -0600

    added comments

--- Travel.cpp
+++ Travel.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 Travel::Travel()
 {
+	//creating two pointers that we'll use later on
     head = NULL;
     user = NULL;
 }
@@ -14,13 +15,16 @@ Travel::~Travel()
     //dtor
 }
 
-
+//Function reads in values from text file and stores them in a linked list
+//First in main.cpp we loop through each line of the text file called locations.txt and store them in variables, 
+//which are passed to the Travel function below
 void Travel::readIn(string location, int nature, int food, int culture, int nightLife, int adventure, int relax){
 
+//Special case for reading in first line of text file (assigning values to head pointer):
     if(head == NULL){
         head = new placeNode;
         head->location = location;
-        head->nature = nature; //converting string to int
+        head->nature = nature; 
         head->food = food;
         head->culture = culture;
         head->nightLife = nightLife;
@@ -28,10 +32,11 @@ void Travel::readIn(string location, int nature, int food, int culture, int nigh
         head->relax = relax;
     }
     else{
+//For all other lines in text create a node with all of the components
         placeNode* currentNode = new placeNode;
 
         currentNode->location = location;
-        currentNode->nature = nature; //converting string to int
+        currentNode->nature = nature; 
         currentNode->food = food;
         currentNode->culture = culture;
         currentNode->nightLife = nightLife;
@@ -43,18 +48,13 @@ void Travel::readIn(string location, int nature, int food, int culture, int nigh
         while(cursor->next != NULL){
             cursor = cursor->next;
         }
-
-        //cout<<currentNode->location<<"->";
-
         cursor->next = currentNode;
-        //currentNode->previous = cursor;
     }
-
-
-
 
 }
 
+//This is the second menu which is called in option 6. Here, we provide the environmental components for the user to rank.
+//Then we store these rankings in a new pointer called "user". Then we use the "user" pointer in a function later on called computePlaceScores.
 void Travel::displayMenu(){
 	user = new userPref;
 	
@@ -83,9 +83,9 @@ user->nightLife = night;
 user->adventure = adventure;
 user->relax = relax;
 
-//cout<<"Or press 'q' to quit"<<endl;
 }
 
+//Print the locations in linked list.
 void Travel::displayPlaces()
 {
     placeNode* temp = new placeNode;
@@ -98,6 +98,7 @@ void Travel::displayPlaces()
        // cout<<" NULL"<<endl;
 }
 
+//We reverse the linked list here. Therefore, when you call the displayPlaces function again, it will print the locations in reverse order.
 void Travel::reversePlaces()
 {
 	placeNode* prev   = NULL;
@@ -113,14 +114,40 @@ void Travel::reversePlaces()
     head = prev;
 }
 
-
+//This function allows the user add a new location and rank each environmental component
 void Travel::addPlace()
 {
 		string newLocation;
+		int nature = 0;
+		int food = 0;
+		int culture = 0;
+		int night = 0;
+		int adventure = 0;
+		int relax = 0;
+		
+		
 		cout << "Enter a city to add: ";
 		cin.ignore();
 		getline(cin, newLocation);
+		
+		cout<<"Please rank the location's environmental components from 1 to 10, 10 being the most representative of the location."<<endl;
+		cout<<"For example, if you added New York City, you would give food and night life a 10, while nature would be closer to a 1"<<endl;
+		
+		cout<<"1. Nature: ";
+		cin >> nature;
+		cout<<"2. Food: ";
+		cin >> food;
+		cout<<"3. Culture/History/Art: ";
+		cin >> culture;
+		cout<<"4. Night Life: ";
+		cin >> night;
+		cout<<"5. Adventure: ";
+		cin >> adventure;
+		cout<<"6. Relax: ";
+		cin >> relax;
 
+		
+		
         placeNode* temp1 = head;
 
         placeNode* added = new placeNode;
@@ -132,11 +159,17 @@ void Travel::addPlace()
         temp1->next = added;
         added->next = NULL;
         added->location = newLocation;
+        added->nature = nature;
+        added->food = food;
+        added->culture = culture;
+        added->nightLife = night;
+        added->adventure = adventure;
+        added->relax = relax;
 
         //return head;
 }
 
-        
+//This function allows the user to remove a location from the list 
 void Travel::removePlace()
 {
     string toDelete;
@@ -149,6 +182,7 @@ void Travel::removePlace()
     placeNode* n = new placeNode;
     n = temp1->next;
 
+//Searching for location to delete
     while(n != NULL){
             if(temp1->location == toDelete){
                 head = n;
@@ -166,6 +200,7 @@ void Travel::removePlace()
         }//end while loop
 }
 
+//Takes the user's score (preferences) and matches it to the rankings already stored for each location to give the best locations for the user to visit
 void Travel::computePlaceScores()
 {
 	int placeScore = 0;
@@ -192,6 +227,7 @@ void Travel::computePlaceScores()
 		
 }
 
+//This function takes the computed place score (from previous function) and reorders the linked list from best to worst 
 void Travel::sortPlaceScores()
 {
 	  struct sortInfo{
